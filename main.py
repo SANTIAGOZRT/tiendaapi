34786eb399d03059e6cacb3356a6988cfe8a093e
Santiago Zarate <santiagozarateacosta@gmail.com>
2025-07-08 19:21:11 -0500

    cors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-# main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.usuarios import ruta_usuarios
 
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes reemplazar "*" con tus dominios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ruta_usuarios)
 
